f419bcca76f26817f273e408632635224615edd6
Mayron Alves <mayroonalves@gmail.com>
2019-12-20 15:52:58 -0300

    troca imagem

--- src/inject/jequitise.js
+++ src/inject/jequitise.js
@@ -1,7 +1,7 @@
 
 (function ($) {
 
-    let logo = 'https://logodownload.org/wp-content/uploads/2017/09/jequiti-logo.png'
+    let logo = 'https://raw.githubusercontent.com/mayroonalves/jequitise/master/images/jequiti-logo.png'
     const jequitise = function () {
         $('body').prepend(`<div id="jequitise" style="background-color: #FFF;width: 100%;height: 100%;position: fixed;z-index: 9999;"><img src="${logo}" style="width: 80%; position: relative; transform: translate(-50%, 50%); left: 50%;" /></div>`)
         $("#jequitise").fadeOut('fast', function(){
