076dd0b82c9b4aae20afdb862e04fc8abd40826c
Mayron Alves de Araujo <mayroonalves@gmail.com>
2022-11-02 16:35:25 -0600

    change the timer to a quicker option

--- src/inject/jequitise.js
+++ src/inject/jequitise.js
@@ -4,16 +4,16 @@
     let logo = 'https://raw.githubusercontent.com/mayroonalves/jequitise/master/images/jequiti-logo.png'
     const jequitise = function () {
         $('body').prepend(`<div id="jequitise" style="background-color: #FFF;width: 100%;height: 100%;position: fixed;z-index: 9999;"><img src="${logo}" style="width: 80%; position: relative; transform: translate(-50%, 50%); left: 50%;" /></div>`)
-        $("#jequitise").fadeOut('fast', function(){
+        $("#jequitise").fadeOut('fast', function () {
             $(this).remove()
         })
     }
 
     const loop = () => {
-        let min = 60000
-        let max = 240000
+        let min = 10 * 1000
+        let max = 60 * 1000
         let rand = Math.ceil(Math.random() * (max - min)) + min
-        setTimeout(function(){
+        setTimeout(function () {
             jequitise()
             loop()
         }, rand)
